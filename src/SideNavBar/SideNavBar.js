25f4415cca52dc95aeeb3f15fa78df4097de3350
Almahn333 <almahn333@gmail.com>
2023-11-14 03:48:53 +0530

    commit 2

--- src/SideNavBar/SideNavBar.js
+++ src/SideNavBar/SideNavBar.js
@@ -2,83 +2,89 @@ import React, { useState } from "react";
 import "./SideNavBar.css";
 
 const SideNavBar = () => {
-	const [isExpanded, setExpendState] = useState(false);
-	const menuItems = [
-		{
-			path: "/",
-			text: "Dashboard",
-			icon: "icons/Dashboard.svg",
-		},
-		{
-			path: "/",
-			text: "Tests",
-			icon: "icons/Tests.svg",
-		},
-		{
-			path: "/",
-			text: "Courses",
-			icon: "icons/Cources.svg",
-		},
+  const [isExpanded, setExpandState] = useState(false);
+  const [activeMenuItem, setActiveMenuItem] = useState(null);
 
-	];
-	return (
-		<div
-			className={
-				isExpanded
-					? "side-nav-container"
-					: "side-nav-container side-nav-container-NX"
-			}
-		>
-			<div className="nav-upper">
-				<div className="nav-heading">
-					{isExpanded && (
-						<div className="nav-brand">
-							<h2>Menu</h2>
-						</div>
-					)}
-					<button
-						className={
-							isExpanded ? "hamburger hamburger-in" : "hamburger hamburger-out"
-						}
-						onClick={() => setExpendState(!isExpanded)}
-					>
-						<span></span>
-						<span></span>
-						<span></span>
-					</button>
-				</div>
-				<div className="nav-menu">
-					{menuItems.map(({ text, icon }) => (
-						<a
-							className={isExpanded ? "menu-item" : "menu-item menu-item-NX"}
-							href="#"
-						>
-							<img className="menu-item-icon" src={icon} alt="" srcset="" />
-							{isExpanded && <p>{text}</p>}
-						</a>
-					))}
-					
-				</div>
-			</div>
-			<div className="nav-footer">
-				{isExpanded && (
-					<div className="nav-details">
-						<img
-							className="nav-footer-avatar"
-							src="icons/user.webp"
-							alt=""
-							srcset=""
-						/>
-						<div className="nav-footer-info">
-							<p className="nav-footer-user-name">Bluberri.</p>
-							<p className="nav-footer-user-position">uxberri@gmail.com</p>
-						</div>
-					</div>
-				)}
-				<img className="sun-icon" src="icons/sun.svg" alt="" srcset="" />
-			</div>
-		</div>
-	);
+  const menuItems = [
+    {
+      path: "/",
+      text: "Dashboard",
+      icon: activeMenuItem === "Dashboard" ? "icons/dashboard1.svg" : "icons/Dashboard.svg",
+    },
+    {
+      path: "/",
+      text: "Tests",
+      icon: activeMenuItem === "Tests" ? "icons/tests1.svg" : "icons/Tests.svg",
+    },
+    {
+      path: "/",
+      text: "Courses",
+      icon: activeMenuItem === "Courses" ? "icons/courses1.svg" : "icons/Cources.svg",
+    },
+  ];
+
+  const handleMenuItemClick = (text) => {
+    setActiveMenuItem(text);
+  };
+
+  return (
+    <div
+      className={
+        isExpanded
+          ? "side-nav-container"
+          : "side-nav-container side-nav-container-NX"
+      }
+    >
+      <div className="nav-upper">
+        <div className="nav-heading">
+          {isExpanded && (
+            <div className="nav-brand">
+              <h2>Menu</h2>
+            </div>
+          )}
+          <button
+            className={
+              isExpanded ? "hamburger hamburger-in" : "hamburger hamburger-out"
+            }
+            onClick={() => setExpandState(!isExpanded)}
+          >
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
+        <div className="nav-menu">
+          {menuItems.map(({ text, icon }) => (
+            <a
+              key={text}
+              className={isExpanded ? "menu-item" : "menu-item menu-item-NX"}
+              href="#"
+              onClick={() => handleMenuItemClick(text)}
+            >
+              <img className="menu-item-icon" src={icon} alt="" />
+              {isExpanded && <p>{text}</p>}
+            </a>
+          ))}
+        </div>
+      </div>
+      <div className="nav-footer">
+        {isExpanded && (
+          <div className="nav-details">
+            <img
+              className="nav-footer-avatar"
+              src="icons/user.webp"
+              alt=""
+            />
+            <div className="nav-footer-info">
+              <p className="nav-footer-user-name">Bluberri.</p>
+              <p className="nav-footer-user-position">uxberri@gmail.com</p>
+            </div>
+          </div>
+        )}
+        <img className="sun-icon" src="icons/sun.svg" alt="" />
+      </div>
+    </div>
+  );
 };
 
 export default SideNavBar;
